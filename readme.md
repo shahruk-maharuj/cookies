d7dbbec45319c5e6ceecdd53b0e35c04323edf5b
Shahruk-Maharuj <shahrukmaharuj10@gmail.com>
2023-08-25 08:19:28 +0600

    cookies banner

---
+++ readme.md
@@ -0,0 +1,7 @@
+<!-- 
+    1. Create Cookie Consent widget
+    2. Fast. Easy. No Coding.
+    3. Responsive Design
+    4. Cookie notification
+    5. Find the ideal layout and position
+ --># cookies
---
+++ top-banner/script.js
@@ -0,0 +1,16 @@
+window.onload = function() {
+    if (!document.cookie.split(';').some((item) => item.trim().startsWith('cookiesAccepted='))) {
+        document.getElementById('cookie-banner').style.display = 'flex';
+    }
+}
+
+document.getElementById('accept').addEventListener('click', function() {
+    var date = new Date();
+    date.setMonth(date.getMonth() + 1); // set the date to 1 month in the future
+    document.cookie = "cookiesAccepted=true; expires=" + date.toUTCString() + "; path=/";
+    document.getElementById('cookie-banner').style.display = 'none';
+});
+
+document.getElementById('decline').addEventListener('click', function() {
+    document.getElementById('cookie-banner').style.display = 'none';
+});
